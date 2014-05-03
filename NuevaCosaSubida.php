3cead05d6789b8286a30c42129e16b76bc748b06
brainsoftsrl <reyksor@hotmail.com>
2014-05-02 20:56:45 -0500

    segunda prueba con un segundo saludo

---
+++ NuevaCosaSubida.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of NuevaCosaSubida
+ *
+ * @author encinas
+ */
+class NuevaCosaSubida {
+    function __construct() {
+        
+    }
+    function saludo2(){
+        echo "holas a todos creo que al fin da el repositorio";
+    }
+}
